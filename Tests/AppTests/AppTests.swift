2783ab883468ed54ad0ac64b02472434e884a9f5
yyokii <yyokii.h@gmail.com>
2021-12-08 23:30:53 +0900

    ✅ Add generate demo md test

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,7 +1,24 @@
 @testable import App
 import XCTVapor
 
+import MarkdownGenerator
+
 final class AppTests: XCTestCase {
+    
+    func testGenerateDemoMarkdown() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+        
+        let job = GenerateAppleDevNewsJob()
+        let content = try await job.generateMdContent()
+        let currentDirectoryPath = FileManager().currentDirectoryPath
+        let mdGenerator = MarkdownFileGenerator(basePath: currentDirectoryPath,
+                                                filename: "demo",
+                                                content: content)
+        try mdGenerator.write()
+    }
+    
     func testHelloWorld() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
